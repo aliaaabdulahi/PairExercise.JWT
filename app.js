cbbb556be86dcf65e0e1ab965d82b63dc249b877
Alia Abdulahi <aliaabdulahicodes@gmail.com>
2021-04-05 16:32:49 -0400

    made jwt work

--- app.js
+++ app.js
@@ -1,14 +1,25 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 app.use(express.json());
 const {
-  models: { User },
-} = require('./db');
-const path = require('path');
+  models: { User, Note },
+} = require("./db");
+const path = require("path");
 
-app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'index.html')));
+async function requireToken(req, res, next) {
+  try {
+    const token = req.headers.authorization;
+    const user = await User.byToken(token);
+    req.user = user;
+    next();
+  } catch (err) {
+    next(err);
+  }
+}
 
-app.post('/api/auth', async (req, res, next) => {
+app.get("/", (req, res) => res.sendFile(path.join(__dirname, "index.html")));
+
+app.post("/api/auth", async (req, res, next) => {
   try {
     res.send({ token: await User.authenticate(req.body) });
   } catch (ex) {
@@ -16,14 +27,33 @@ app.post('/api/auth', async (req, res, next) => {
   }
 });
 
-app.get('/api/auth', async (req, res, next) => {
+app.get("/api/auth", requireToken, async (req, res, next) => {
   try {
-    res.send(await User.byToken(req.headers.authorization));
+    console.log(requireToken());
+    res.send(req.user);
   } catch (ex) {
     next(ex);
   }
 });
 
+app.get("/api/users/:id/notes", requireToken, async (req, res, next) => {
+  try {
+    const user = req.user;
+    console.log(user);
+    const userWithNotes = await User.findByPk(req.params.id, {
+      include: Note,
+    });
+    // res.send(userWithNotes.notes);
+
+    if (Number(req.params.id) === user.id) {
+      res.send(userWithNotes.notes);
+    } else {
+      res.send("can't access this user's notes!");
+    }
+  } catch (ex) {
+    next(ex);
+  }
+});
 app.use((err, req, res, next) => {
   console.log(err);
   res.status(err.status || 500).send({ error: err.message });
--- db.js
+++ db.js
@@ -1,25 +1,31 @@
-const Sequelize = require('sequelize');
+const Sequelize = require("sequelize");
 const { STRING } = Sequelize;
 const config = {
   logging: false,
 };
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
 const SECRET_KEY = process.env.JWT;
-const bcrypt = require('bcrypt');
-const saltRounds = 10;
+const bcrypt = require("bcrypt");
 
 if (process.env.LOGGING) {
   delete config.logging;
 }
 const conn = new Sequelize(
-  process.env.DATABASE_URL || 'postgres://localhost/acme_db',
+  process.env.DATABASE_URL || "postgres://localhost/acme_db",
   config
 );
 
-const User = conn.define('user', {
+const User = conn.define("user", {
   username: STRING,
   password: STRING,
 });
+
+const Note = conn.define("note", {
+  text: STRING,
+});
+
+User.hasMany(Note);
+Note.belongsTo(User);
 
 User.byToken = async (token) => {
   try {
@@ -29,53 +35,67 @@ User.byToken = async (token) => {
       return user;
     }
   } catch (ex) {
-    const error = Error('bad credentials');
+    const error = Error("bad credentials");
     error.status = 401;
     throw error;
   }
 };
 
 User.authenticate = async ({ username, password }) => {
+  const hashed = await bcrypt.hash(password, 10);
+  const correct = await bcrypt.compare(password, hashed);
   const user = await User.findOne({
     where: {
       username,
-      password,
     },
   });
-
-  if (user) {
+  if (user && correct) {
     return jwt.sign({ userId: user.id, username: user.username }, SECRET_KEY);
   }
-  const error = Error('bad credentials');
+  const error = Error("bad credentials");
   error.status = 401;
   throw error;
 };
 
-User.beforeCreate((user) => {
-  return bcrypt.hash(user.password, saltRounds, async function (err, hash) {
-    try {
-      return await (user.password = hash);
-    } catch (err) {
-      console.log(err);
-    }
-  });
+User.beforeCreate(async (user) => {
+  const hashed = await bcrypt.hash(user.password, 10);
+  user.password = hashed;
 });
 
 const syncAndSeed = async () => {
   await conn.sync({ force: true });
   const credentials = [
-    { username: 'lucy', password: 'lucy_pw' },
-    { username: 'moe', password: 'moe_pw' },
-    { username: 'larry', password: 'larry_pw' },
+    { username: "lucy", password: "lucy_pw" },
+    { username: "moe", password: "moe_pw" },
+    { username: "larry", password: "larry_pw" },
+  ];
+  const notes = [
+    { text: "Remember to buy cheese" },
+    { text: "Study Sequelize!" },
+    { text: "Movie night at 8" },
   ];
   const [lucy, moe, larry] = await Promise.all(
     credentials.map((credential) => User.create(credential))
   );
+
+  const [note1, note2, note3] = await Promise.all(
+    notes.map((note) => Note.create(note))
+  );
+
+  note1.setUser(moe);
+  note2.setUser(lucy);
+  note3.setUser(larry);
+
   return {
     users: {
       lucy,
       moe,
       larry,
+    },
+    notes: {
+      note1,
+      note2,
+      note3,
     },
   };
 };
@@ -84,5 +104,6 @@ module.exports = {
   syncAndSeed,
   models: {
     User,
+    Note,
   },
 };
